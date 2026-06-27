e95cfe70b0162352147b34d14f3e016bfa9e0ba7
Morten Rasmussen <mortenr90@outlook.com>
2026-06-27 19:55:35 +0200

    Created methods for the cases that still had command handlers directly inside them in the switch, next time I will create a new class for them.

--- TaskTracker_CLI/Program.cs
+++ TaskTracker_CLI/Program.cs
@@ -58,103 +58,23 @@ namespace TaskTracker_CLI
             switch (args[0].ToLower())
             {
                 case "list":
-                        GetList(tasks);
-                        break;
+                    GetList(tasks);
+                    break;
 
                 case "add":
-                        AddCommand(args, taskManager, tasks, taskStorage);
-                        break;
+                    AddCommand(args, taskManager, tasks, taskStorage);
+                    break;
 
                 case "update":
-                    {
-                        if (args.Length < 2)
-                        {
-                            Console.WriteLine("Usage: <Id> <description>");
-                            break;
-                        }
-
-                        string description = args[2];
-
-                        if (!int.TryParse(args[1], out int updateId))
-                        {
-                            Console.WriteLine("Invalid task ID.");
-                            break;
-                        }
-
-                        var updateTask = taskManager.FindTaskById(tasks, updateId);
-
-                        if (updateTask != null)
-                        {
-                            taskManager.UpdateTask(updateTask, description);
-                            Console.WriteLine("Task updated succesfully!");
-
-                            taskStorage.SaveFile(tasks);
-                        }
-                        else
-                            Console.WriteLine("Task ID does not exist!");
-                    }
+                    UpdateCommand(args, taskManager, tasks, taskStorage);
                     break;
 
                 case "delete":
-                    {
-                        if (args.Length < 2)
-                        {
-                            Console.WriteLine("Usage: <Id> <description>");
-                            break;
-                        }
-
-                        if (!int.TryParse(args[1], out int deleteId))
-                        {
-                            Console.WriteLine("Invalid task ID.");
-                            break;
-                        }
-
-                        var deleteTask = taskManager.FindTaskById(tasks, deleteId);
-
-                        if (deleteTask != null)
-                        {
-                            taskManager.RemoveTask(tasks, deleteTask);
-                            Console.WriteLine("Task succesfully removed!");
-
-                            taskStorage.SaveFile(tasks);
-                        }
-                        else
-                            Console.WriteLine("Task ID does not exist!");
-                    }
+                    DeleteCommand(args, taskManager, tasks, taskStorage);
                     break;
 
                 case "mark":
-                    {
-                        if (args.Length < 2)
-                        {
-                            Console.WriteLine("Usage: <Id> <description>");
-                            break;
-                        }
-
-                        if (!int.TryParse(args[1], out int markId))
-                        {
-                            Console.WriteLine("Invalid task ID.");
-                            break;
-                        }
-
-                        var markTask = taskManager.FindTaskById(tasks, markId);
-
-                        if (markTask != null)
-                        {
-                            string statusInput = (string)args[2];
-
-                            if (Enum.TryParse<TaskStatus>(statusInput, true, out TaskStatus status))
-                            {
-                                taskManager.MarkTask(markTask, status);
-                                Console.WriteLine("Task status succesfully changed!");
-                                taskStorage.SaveFile(tasks);
-                            }
-                            else
-                                Console.WriteLine("Please enter \"ToDo\", \"InProgress\" or \"Done\".");
-                        }
-                        else
-                            Console.WriteLine("Task ID does not exist!");
-                    }
+                    MarkCommand(args, taskManager, tasks, taskStorage);
                     break;
 
                 case "help":
@@ -164,7 +84,6 @@ namespace TaskTracker_CLI
                     Console.WriteLine("delete <id>");
                     Console.WriteLine("mark <id> <ToDo|InProgress|Done>");
                     break;
-
 
                 default:
                     Console.WriteLine("Unknown command.");
@@ -180,26 +99,124 @@ namespace TaskTracker_CLI
 
         private static void AddCommand(string[] args, TaskManager taskManager, List<ToDoItem> tasks, TaskStorage taskStorage)
         {
-            bool addCommandLoop = true;
-            while (addCommandLoop)
+            if (args.Length < 2)
             {
-                if (args.Length < 2)
-                {
-                    Console.WriteLine("Usage: Add <description>");
-                    break;
-                }
+                Console.WriteLine("Usage: Add <description>");
+                return;
+            }
 
-                string description = string.Join(" ", args.Skip(1));
+            string description = string.Join(" ", args.Skip(1));
 
-                if (string.IsNullOrWhiteSpace(description))
-                {
-                    Console.WriteLine("Task description cannot be empty!");
-                }
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                Console.WriteLine("Task description cannot be empty!");
+                return;
+            }
 
-                taskManager.AddTask(tasks, description);
+            taskManager.AddTask(tasks, description);
+            taskStorage.SaveFile(tasks);
+
+            Console.WriteLine("Task added succesfully!");
+        }
+
+        private static void UpdateCommand(string[] args, TaskManager taskManager, List<ToDoItem> tasks, TaskStorage taskStorage)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: <Id> <description>");
+                return;
+            }
+
+            string description = string.Join(" ", args.Skip(2));
+
+            if (!int.TryParse(args[1], out int updateId))
+            {
+                Console.WriteLine("Invalid task ID.");
+                return;
+            }
+
+            var updateTask = taskManager.FindTaskById(tasks, updateId);
+
+            if (updateTask != null)
+            {
+                taskManager.UpdateTask(updateTask, description);
+                Console.WriteLine("Task updated succesfully!");
+
                 taskStorage.SaveFile(tasks);
-                Console.WriteLine("Task added succesfully!");
-                addCommandLoop = false;
+            }
+            else
+            {
+                Console.WriteLine("Task ID does not exist!");
+                return;
+            }  
+        }
+
+        private static void DeleteCommand(string[] args, TaskManager taskManager, List<ToDoItem> tasks, TaskStorage taskStorage)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: <Id> <description>");
+                return;
+            }
+
+            if (!int.TryParse(args[1], out int deleteId))
+            {
+                Console.WriteLine("Invalid task ID.");
+                return;
+            }
+
+            var deleteTask = taskManager.FindTaskById(tasks, deleteId);
+
+            if (deleteTask != null)
+            {
+                taskManager.RemoveTask(tasks, deleteTask);
+                Console.WriteLine("Task succesfully removed!");
+
+                taskStorage.SaveFile(tasks);
+            }
+            else
+            {
+                Console.WriteLine("Task ID does not exist!");
+                return;
+            }
+        }
+
+        private static void MarkCommand(string[] args, TaskManager taskManager, List<ToDoItem> tasks, TaskStorage taskStorage)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: <Id> <description>");
+                return;
+            }
+
+            if (!int.TryParse(args[1], out int markId))
+            {
+                Console.WriteLine("Invalid task ID.");
+                return;
+            }
+
+            var markTask = taskManager.FindTaskById(tasks, markId);
+
+            if (markTask != null)
+            {
+                string statusInput = (string)args[2];
+
+                if (Enum.TryParse<TaskStatus>(statusInput, true, out TaskStatus status))
+                {
+                    taskManager.MarkTask(markTask, status);
+                    Console.WriteLine("Task status succesfully changed!");
+                    taskStorage.SaveFile(tasks);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter \"ToDo\", \"InProgress\" or \"Done\".");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Task ID does not exist!");
+                return;
             }
         }
     }
